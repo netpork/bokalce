6cc436a7963b9fc4e310429d4bb443e681533e63
netpork <netpork@gmail.com>
2014-11-30 01:22:34 +0100

    Kalms hints; fixed point

--- java/tk/netpork/bokalce/Rotozoom.java
+++ java/tk/netpork/bokalce/Rotozoom.java
@@ -17,6 +17,9 @@ public class Rotozoom {
 
     private static final int textureWidth = 128;
     private static final int textureHeight = 128;
+    private final int textureWidthMask = textureWidth - 1;
+    private final int textureHeightMask = textureHeight - 1;
+
 
     private int[] pixels = new int[Video.width * Video.height];
 
@@ -26,54 +29,33 @@ public class Rotozoom {
     }
 
     public void update(Canvas canvas) {
-        double z, a, dx, dy, px, py, u, v;
+        rotoZoomFixed();
+        mVideo.mBitmap.setPixels(pixels, 0, Video.width, 0, 0, Video.width, Video.height);
+    }
 
+    private void rotoZoom() {
+        double z, a, dx, dy, px, py, u, v;
+        int offset = 0;
         z = 1 + Math.sin(zoom);
         a = Math.PI * Math.sin(angle);
         dx = z * Math.cos(a);
         dy = z * Math.sin(a);
 
-//        Log.i(TAG, "---dx: " + dx + " dy: " + dy);
-
         px = ((bx + textureWidth / 2 + Video.width / 2 * dx + Video.height / 2 * dy) % textureWidth);
         py = ((by + textureHeight / 2 - Video.width / 2 * dy + Video.height / 2 * dx) % textureHeight);
         u = (-Video.width * dx + dy);
         v = (-Video.width * dy - dx);
-//        Log.i(TAG, "---px: " + px + " py: " + py);
-
 
         for (int y = 0; y < Video.height; y++) {
             for (int x = 0; x < Video.width; x++) {
-                pixels[y * Video.width + x] = Video.spritePixels[((int) py & textureHeight - 1) * textureHeight + ((int) px & textureWidth - 1)];
+//                pixels[y * Video.width + x] = Video.spritePixels[((int) py & textureWidthMask) * textureHeight + ((int) px & textureHeightMask)];
+                pixels[offset] = Video.spritePixels[((int) py & textureWidthMask) * textureHeight + ((int) px & textureHeightMask)];
                 px -= dx;
                 py += dy;
-
-                while (px < 0) {
-                    px += textureWidth;
-                }
-                while (py < 0) {
-                    py += textureHeight;
-                }
-
-                px %= textureWidth;
-                py %= textureHeight;
+                offset++;
             }
-
-//            px -= (-Video.width * dx + dy);
-//            py += (-Video.width * dy - dx);
-
             px -= u;
             py += v;
-
-            while (px < 0) {
-                px += textureWidth;
-            }
-            while (py < 0) {
-                py += textureHeight;
-            }
-
-            px %= textureWidth;
-            py %= textureHeight;
         }
 
         angle += 0.04;
@@ -82,9 +64,43 @@ public class Rotozoom {
 //        bx += 3;
 //        by += 5;
 
-
-        mVideo.mBitmap.setPixels(pixels, 0, Video.width, 0, 0, Video.width, Video.height);
     }
 
+    private void rotoZoomFixed() {
+        double z, a, dx, dy, px, py;
+        int offset = 0, u, v;
+        z = 1 + Math.sin(zoom);
+        a = Math.PI * Math.sin(angle);
+        dx = z * Math.cos(a);
+        dy = z * Math.sin(a);
 
+        px = ((bx + textureWidth / 2 + Video.width / 2 * dx + Video.height / 2 * dy) % textureWidth);
+        py = ((by + textureHeight / 2 - Video.width / 2 * dy + Video.height / 2 * dx) % textureHeight);
+
+        int dxFixed = (int) (dx * 65536.0);
+        int dyFixed = (int) (dy * 65536.0);
+        int pxFixed = (int) (px * 65536.0);
+        int pyFixed = (int) (py * 65536.0);
+
+        u = (-Video.width * dxFixed + dyFixed);
+        v = (-Video.width * dyFixed - dxFixed);
+
+        for (int y = 0; y < Video.height; y++) {
+            for (int x = 0; x < Video.width; x++) {
+                pixels[offset] = Video.spritePixels[((pyFixed >> 16) & textureWidthMask) * textureHeight + ((pxFixed >> 16) & textureHeightMask)];
+                pxFixed -= dxFixed;
+                pyFixed += dyFixed;
+                offset++;
+            }
+            pxFixed -= u;
+            pyFixed += v;
+        }
+
+        angle += 0.04;
+        zoom += 0.1;
+
+        bx += 3;
+        by += 5;
+
+    }
 }
